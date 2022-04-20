fb6c1e805b39b9ca6fc3c91900dd06a6c561043c
mauerbauertraurigkeit <nour20hk@gmail.com>
2022-04-20 23:21:19 +0300

    Player Creation

--- index.js
+++ index.js
@@ -13,6 +13,21 @@ c.fillRect(0, 0, canvas.width, canvas.height)
 const image = new Image()
 image.src = 'img/PelletTown.png'
 
+const playerImage = new Image()
+playerImage.src = 'img/playerDown.png'
+
 image.onload = () => {
-    c.drawImage(image, 0, 0 )
+    c.drawImage(image, -750, -610)
+    c.drawImage(
+        playerImage, 
+        0,
+        0,
+        playerImage.width/4,
+        playerImage.height,
+        canvas.width/2 - (playerImage.width/4)/2, 
+        canvas.height/2 - playerImage.height/2,
+        playerImage.width/4,
+        playerImage.height
+    )
 }
+
