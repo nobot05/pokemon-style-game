a2fed0d36f5154f8733031bc14a4dbd8940e814d
mauerbauertraurigkeit <nour20hk@gmail.com>
2022-04-25 23:36:11 +0300

    Drawing Collisions on Map

--- index.js
+++ index.js
@@ -64,15 +64,44 @@ const playerImage = new Image();
 playerImage.src = "img/playerDown.png";
 
 class Sprite {
-  constructor({ position, velocit, image }) {
+  constructor({ position, velocity, image, frames = { max: 1 } }) {
     this.position = position;
     this.image = image;
+    this.frames = frames;
+    this.image.onload = () => {
+      this.width = this.image.width / this.frames.max;
+      this.height = this.image.height;
+      console.log(this.width);
+      console.log(this.height);
+    };
   }
 
   draw() {
-    c.drawImage(this.image, this.position.x, this.position.y);
+    // c.drawImage(this.image, this.position.x, this.position.y);
+    c.drawImage(
+      this.image,
+      0,
+      0,
+      this.image.width / this.frames.max,
+      this.image.height,
+      this.position.x,
+      this.position.y,
+      this.image.width / this.frames.max,
+      this.image.height
+    );
   }
 }
+
+const player = new Sprite({
+  position: {
+    x: canvas.width / 2 - 192 / 4 / 2,
+    y: canvas.height / 2 - 68 / 2,
+  },
+  image: playerImage,
+  frames: {
+    max: 4,
+  },
+});
 
 const background = new Sprite({
   position: {
@@ -97,37 +126,42 @@ const keys = {
   },
 };
 
-const testBoundary = new Boundary({
-  position: {
-    x: 400,
-    y: 400,
-  },
-});
+// const testBoundary = new Boundary({
+//   position: {
+//     x: 400,
+//     y: 400,
+//   },
+// });
 
-const movables = [background, testBoundary];
+const movables = [background, ...boundaries];
+
+function rectangularCollision({ rectangle1, rectangle2 }) {
+  return (
+    rectangle1.position.x + rectangle1.width >= rectangle2.position.x &&
+    rectangle1.position.x <= rectangle2.position.x + rectangle2.width &&
+    rectangle1.position.y <= rectangle2.position.y + rectangle2.height &&
+    rectangle1.position.y + rectangle1.height >= rectangle2.position.y
+  );
+}
 
 function animate() {
   window.requestAnimationFrame(animate);
   //console.log('animate')
   background.draw();
-  // boundaries.forEach((boundary) => {
-  //   boundary.draw();
-  // });
-  testBoundary.draw();
-  c.drawImage(
-    playerImage,
-    0,
-    0,
-    playerImage.width / 4,
-    playerImage.height,
-    canvas.width / 2 - playerImage.width / 4 / 2,
-    canvas.height / 2 - playerImage.height / 2,
-    playerImage.width / 4,
-    playerImage.height
-  );
+  boundaries.forEach((boundary) => {
+    boundary.draw();
 
-  if (player.position.x + player.width) {
-  }
+    if (
+      rectangularCollision({
+        rectangle1: player,
+        rectangle2: boundary,
+      })
+    ) {
+      console.log("colliding");
+    }
+  });
+  // testBoundary.draw();
+  player.draw();
 
   if (keys.w.pressed && lastKey === "w") {
     movables.forEach((movable) => {
