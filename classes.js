367e9a6504193140ed86f5d2d6a08de13e2833f0
mauerbauertraurigkeit <nour20hk@gmail.com>
2022-05-17 10:15:14 +0300

    Foreground Objects

---
+++ classes.js
@@ -0,0 +1,42 @@
+class Sprite {
+  constructor({ position, velocity, image, frames = { max: 1 } }) {
+    this.position = position;
+    this.image = image;
+    this.frames = frames;
+    this.image.onload = () => {
+      this.width = this.image.width / this.frames.max;
+      this.height = this.image.height;
+      console.log(this.width);
+      console.log(this.height);
+    };
+  }
+
+  draw() {
+    // c.drawImage(this.image, this.position.x, this.position.y);
+    c.drawImage(
+      this.image,
+      0,
+      0,
+      this.image.width / this.frames.max,
+      this.image.height,
+      this.position.x,
+      this.position.y,
+      this.image.width / this.frames.max,
+      this.image.height
+    );
+  }
+}
+
+class Boundary {
+  static width = 48;
+  static height = 48;
+  constructor({ position }) {
+    this.position = position;
+    this.width = 48; //the block was 12x2 but then we zoomed our map to 400% so 12x4=48
+    this.height = 48;
+  }
+  draw() {
+    c.fillStyle = "rgba(255, 0, 0, 0.0)";
+    c.fillRect(this.position.x, this.position.y, this.width, this.height);
+  }
+}
--- index.js
+++ index.js
@@ -19,20 +19,6 @@ for (let i = 0; i < collisions.length; i += 70) {
 }
 //console.log(collisionsMap);
 
-class Boundary {
-  static width = 48;
-  static height = 48;
-  constructor({ position }) {
-    this.position = position;
-    this.width = 48; //the block was 12x2 but then we zoomed our map to 400% so 12x4=48
-    this.height = 48;
-  }
-  draw() {
-    c.fillStyle = "rgba(255, 0, 0, 0.0)";
-    c.fillRect(this.position.x, this.position.y, this.width, this.height);
-  }
-}
-
 const boundaries = [];
 
 const offset = {
@@ -60,37 +46,11 @@ console.log(boundaries);
 const image = new Image();
 image.src = "img/PelletTown.png";
 
+const foregroundImage = new Image();
+foregroundImage.src = "img/foregroundObjects.png";
+
 const playerImage = new Image();
 playerImage.src = "img/playerDown.png";
-
-class Sprite {
-  constructor({ position, velocity, image, frames = { max: 1 } }) {
-    this.position = position;
-    this.image = image;
-    this.frames = frames;
-    this.image.onload = () => {
-      this.width = this.image.width / this.frames.max;
-      this.height = this.image.height;
-      console.log(this.width);
-      console.log(this.height);
-    };
-  }
-
-  draw() {
-    // c.drawImage(this.image, this.position.x, this.position.y);
-    c.drawImage(
-      this.image,
-      0,
-      0,
-      this.image.width / this.frames.max,
-      this.image.height,
-      this.position.x,
-      this.position.y,
-      this.image.width / this.frames.max,
-      this.image.height
-    );
-  }
-}
 
 const player = new Sprite({
   position: {
@@ -109,6 +69,14 @@ const background = new Sprite({
     y: offset.y,
   },
   image: image,
+});
+
+const foreground = new Sprite({
+  position: {
+    x: offset.x,
+    y: offset.y,
+  },
+  image: foregroundImage,
 });
 
 const keys = {
@@ -133,7 +101,7 @@ const keys = {
 //   },
 // });
 
-const movables = [background, ...boundaries];
+const movables = [background, ...boundaries, foreground];
 
 function rectangularCollision({ rectangle1, rectangle2 }) {
   return (
@@ -153,6 +121,7 @@ function animate() {
   });
   // testBoundary.draw();
   player.draw();
+  foreground.draw()
 
   let moving = true;
   if (keys.w.pressed && lastKey === "w") {
